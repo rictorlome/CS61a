21fd7723ede6d93d92e6c16e2eefa5eadecb9a50
rictorlome <33004639+rictorlome@users.noreply.github.com>
2017-12-09 19:53:54 -0500

    Create cs61a_lab03.py

---
+++ cs61a_lab03.py
@@ -0,0 +1,46 @@
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    print(n)
+    counter = 1
+    if n == 1:
+        return counter
+    if n % 2 == 0:
+        counter += hailstone(n//2)
+    if n % 2 == 1:
+        counter += hailstone(n*3+1)
+    return counter
+
+
+def ab_plus_c(a, b, c):
+    """Computes a * b + c.
+
+    >>> ab_plus_c(2, 4, 3)  # 2 * 4 + 3
+    11
+    >>> ab_plus_c(0, 3, 2)  # 0 * 3 + 2
+    2
+    >>> ab_plus_c(3, 0, 2)  # 3 * 0 + 2
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    if a == 0 or b == 0:
+        return c
+    if a == 1:
+        return b + c
+    if b == 1:
+        return a + c
+    else:
+        return ab_plus_c(a-1, b, c) + b
