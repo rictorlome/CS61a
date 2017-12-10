bb0bab70fd57b6c8856e86c7cd684b6950328fda
rictorlome <33004639+rictorlome@users.noreply.github.com>
2017-12-10 17:08:21 -0500

    Create cs61a_hw03.py

---
+++ cs61a_hw03.py
@@ -0,0 +1,87 @@
+##https://inst.eecs.berkeley.edu/~cs61a/fa16/hw/hw03/
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    """
+    Do not use any assignment statements - use recursion instead:
+    """
+    "*** YOUR CODE HERE ***"
+    if k < 10:
+        return k == 7
+    else:
+        while k//10 > 0:
+            if k%10 == 7:
+                return True
+            else:
+                return has_seven(k//10)
+
+def summation(n, term):
+
+    """Return the sum of the first n terms in the sequence defined by term.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return term(n)
+    else:
+        return term(n) + summation(n-1,term)
+
+
+from operator import add, mul
+
+def square(x):
+    return x * x
+
+def triple(x):
+    return 3 * x
+
+def identity(x):
+    return x
+
+def increment(x):
+    return x + 1
+
+def accumulate(combiner, base, n, term):
+    """Return the result of combining the first n terms in a sequence and base.
+    The terms to be combined are term(1), term(2), ..., term(n).  combiner is a
+    two-argument commutative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)   # 2 * 1^2 * 2^2 * 3^2
+    72
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return base
+    elif n == 1:
+        return combiner(base, term(n))
+    else:
+        return combiner(term(n),accumulate(combiner,base, n-1, term))
